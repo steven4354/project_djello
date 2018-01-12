b1ee135bbad18e27d2513c52c4600a0a01264e6f
Steven Li <steven4354@gmail.com>
2018-01-12 13:55:56 -0800

    react set up

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
+import "./index.css";
 
-import { createStore, applyMiddleware } from "redux";
-import { reducers } from "./reducers";
-import { Provider } from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import {reducers} from "./reducers";
+import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 
 let store = createStore(reducers, applyMiddleware(thunk));
